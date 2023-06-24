45fac1584bbe63d18ca175bfd562cab265fb1213
斎藤正大 <masahiro@saitoumasahironoMacBook-Pro.local>
2023-06-24 19:17:48 +0900

    update delete lib shellscript

--- bash/delete_lib.sh
+++ bash/delete_lib.sh
@@ -1,27 +1,30 @@
 #!/bin/sh
 
-# check argument
+# Check argument
 if [ $# -ne 1 ]; then
   echo "Need only one argument for directory name."
   exit 1
 fi
 
-# handle command line argument
+# Handle to command line argument
 LIB_NAME=$1
 HOME_DIR=/design_pattern
 
-# delete directory
+# Changeble valiable
+FILE=""
+
+# Delete directory
 rm -rf $HOME_DIR/src/$LIB_NAME
 
-# delete line from $HOME_DIR/src/CMakeLists.txt
+# Delete line from $HOME_DIR/src/CMakeLists.txt
 FILE="$HOME_DIR/src/CMakeLists.txt"
 sed -i -e "/add_subdirectory($LIB_NAME)/d" $FILE
 sed -i -e "/mod/d" $FILE
 
-# delete line from $HOME_DIR/test/CMakeLists.txt
+# Delete line from $HOME_DIR/test/CMakeLists.txt
 FILE=$HOME_DIR/test/CMakeLists.txt
 sed -i -e /$LIB_NAME/d $FILE
 
-# delete $HOME_DIR/test/$LIB_NAME_test.cc
+# Delete $HOME_DIR/test/$LIB_NAME_test.cc
 FILE=$HOME_DIR/test/$LIB_NAME'_test.cc'
 rm -f $FILE
