6a425c53e558a0df03c0fb84d561fc241a2692d7
斎藤正大 <masahiro@saitoumasahironoMacBook-Pro.local>
2023-06-24 19:07:32 +0900

    add delete lib shell script

---
+++ bash/delete_lib.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# check argument
+if [ $# -ne 1 ]; then
+  echo "Need only one argument for directory name."
+  exit 1
+fi
+
+# handle command line argument
+LIB_NAME=$1
+HOME_DIR=/design_pattern
+
+# delete directory
+rm -rf $HOME_DIR/src/$LIB_NAME
+
+# delete line from $HOME_DIR/src/CMakeLists.txt
+FILE="$HOME_DIR/src/CMakeLists.txt"
+sed -i -e "s/^.*add_subdirectory($LIB_NAME)//d" $FILE
+sed -i -e "/mod/d" $FILE
+
+# delete line from $HOME_DIR/test/CMakeLists.txt
+FILE=$HOME_DIR/test/CMakeLists.txt
+sed -i -e /$LIB_NAME/d $FILE
+
+# delete $HOME_DIR/test/$LIB_NAME_test.cc
+FILE=$HOME_DIR/test/$LIB_NAME'_test.cc'
+rm -f $FILE
