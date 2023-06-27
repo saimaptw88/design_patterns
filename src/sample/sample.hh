7021dce8855c7acee8c19913044ea1f42d814268
斎藤正大 <masahiro@saitoumasahironoMacBook-Pro.local>
2023-06-27 22:01:36 +0900

    add comment

--- src/sample/sample.hh
+++ src/sample/sample.hh
@@ -2,7 +2,17 @@
 #ifndef SRC_SAMPLE_SAMPLE_HH_
 #define SRC_SAMPLE_SAMPLE_HH_
 
+/**
+ * @namespace Sample
+ * @brief For sample
+ * @details Just for sample
+*/
 namespace Sample {
+/**
+ * @fn int sum
+ * @brief add int and int
+ * @return int
+*/
 int sum(int, int);
 }  // namespace Sample
 
